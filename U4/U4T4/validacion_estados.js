576dab0ac476d4ca8590cd88aba5a556897bc36f
davidrodriguezm <alum.drodriguezm@iesalixar.org>
2020-11-25 20:05:58 +0100

    tarea 4

--- U4/U4T4/validacion_estados.js
+++ U4/U4T4/validacion_estados.js
@@ -8,12 +8,12 @@ function cam_grupo( cad ) {
     let resul = false;
     if( cad.length == 20 ) {
         resul = true;
-        document.getElementById("grup").class = ""
-        document.getElementById("para_grup").class = ""
+        document.getElementById("grup").className = "";
+        document.getElementById("para_grup").className = "";
     } else {
-        alert("El campo nombre tiene que ser de 20 caracteres")
-        document.getElementById("grup").class = "rojo"
-        document.getElementById("para_grup").class = "rojo"
+        alert("El campo nombre tiene que ser de 20 caracteres");
+        document.getElementById("grup").className = "rojo";
+        document.getElementById("para_grup").className = "rojo";
     }
     return resul;
 }
@@ -22,12 +22,12 @@ function cam_nombre( cad ) {
     let resul = false;
     if( cad.length == 20 ) {
         resul = true;
-        document.getElementById("nom").class = ""
-        document.getElementById("para_nom").class = ""
+        document.getElementById("nom").className = "";
+        document.getElementById("para_nom").className = "";
     } else {
-        alert("El campo nombre tiene que ser de 20 caracteres")
-        document.getElementById("nom").class = "rojo"
-        document.getElementById("para_nom").class = "rojo"
+        alert("El campo nombre tiene que ser de 20 caracteres");
+        document.getElementById("nom").className = "rojo";
+        document.getElementById("para_nom").className = "rojo";
     }
     return resul;
 }
@@ -36,12 +36,12 @@ function com_loc( num ) {
     let resul = false;
     if( !isNaN(num) || num === "" ) {
         resul = true;
-        document.getElementById("loc").class = ""
-        document.getElementById("para_loc").class = ""
+        document.getElementById("loc").className = "";
+        document.getElementById("para_loc").className = "";
     } else {
-        alert("La localizacion tiene que ser de numero o vacio")
-        document.getElementById("loc").class = "rojo"
-        document.getElementById("para_loc").class = "rojo"
+        alert("La localizacion tiene que ser de numero o vacio");
+        document.getElementById("loc").className = "rojo";
+        document.getElementById("para_loc").className = "rojo";
     }
     return resul;
 }
@@ -50,21 +50,23 @@ function com_year( num ) {
     let resul = false;
     if( !isNaN(num) && num.length == 4) {
         resul = true;
-        document.getElementById("ano").class = ""
-        document.getElementById("para_ano").class = ""
+        document.getElementById("ano").className = "";
+        document.getElementById("para_ano").className = "";
     } else {
-        alert("El año tiene que ser de 4 caracteres numericos")
-        document.getElementById("ano").class = "rojo"
-        document.getElementById("para_ano").class = "rojo"
+        alert("El año tiene que ser de 4 caracteres numericos");
+        document.getElementById("ano").className = "rojo";
+        document.getElementById("para_ano").className = "rojo";
     }
     return resul;
 }
 
-function validar() {
+function validar( e ) {
     let resul = false;
     if( cam_nombre( document.getElementById("nom").value ) && cam_grupo( document.getElementById("grup").value ) 
         && com_loc( document.getElementById("loc").value ) && com_year( document.getElementById("ano").value) ) {
         resul = true;
-    } 
+    } else {
+        e.preventDefault();
+    }
     return resul;
 }
