f2ba02edf68f319f42348b63bd51a15cfe5e1c6e
davidrodriguezm <alum.drodriguezm@iesalixar.org>
2021-02-07 03:06:24 +0100

    tarea u7 t1

---
+++ U7/U7T1/u7t1.js
@@ -0,0 +1,69 @@
+function area_session() {
+    let contenido = '';
+    for (let i = 0; i < sessionStorage.length; i++) { 
+        let clave = sessionStorage.key(i);
+        let valor = sessionStorage.getItem(clave);
+        if ( clave != 'IsThisFirstTime_Log_From_LiveServer') contenido += clave + ' - ' + valor + '\n';
+    }
+    document.getElementById('area_session').innerHTML = contenido;
+}
+
+function area_local() {
+    let contenido = '';
+    for (let i = 0; i < localStorage.length; i++) { 
+        let clave = localStorage.key(i);
+        let valor = localStorage.getItem(clave);
+        if ( clave != 'IsThisFirstTime_Log_From_LiveServer') contenido += clave + ' - ' + valor + '\n';
+    }
+    document.getElementById('area_local').innerHTML = contenido;
+}
+
+function guardar() {
+    let tipo_al = document.getElementById('almacena').value;
+    let clave = document.getElementById('clave').value;
+    let valor = document.getElementById('valor').value;
+    if ( document.getElementById('valor').value != "" && document.getElementById('clave').value != "" ) {
+        if ( tipo_al == "session" ) {
+            sessionStorage.setItem(clave, valor); 
+            area_session();      
+        } else if ( tipo_al == "local" ) {
+            localStorage.setItem(clave, valor);
+            area_local();
+        }
+    } else {
+        alert('Introduce una clave y un valor');
+    }
+    
+}
+
+function refrescar() {
+    area_session();
+    area_local();
+}
+
+function eliminar() {
+    if ( localStorage.length > 0  && sessionStorage.length > 0  ) {
+        sessionStorage.clear();
+        localStorage.clear();
+        refrescar();
+    } else {
+        alert('No hay datos almacenados. Introduce una clave y un valor');
+    }
+}
+
+function compatibilidad() {
+    if ( window.sessionStorage !== "undefined" && window.localStorage !== "undefined" ) {
+        alert('Tu navegador acepta almacenamiento local y por sesión');
+    } else if( window.sessionStorage !== "undefined" ) {
+        alert('Tu navegador acepta almacenamiento por sesión y no por local');
+    } else if( window.localStorage !== "undefined" ) {
+        alert('Tu navegador acepta almacenamiento local y no por sesión');
+    } else {
+        alert('Tu navegador no acepta almacenamiento local ni por sesión');
+    }
+}
+
+document.getElementById('guardar').addEventListener('click', guardar, false);
+document.getElementById('eliminar').addEventListener('click', eliminar, false);
+document.getElementById('refrescar').addEventListener('click', refrescar, false);
+document.getElementById('compatibilidad').addEventListener('click', compatibilidad, false);
